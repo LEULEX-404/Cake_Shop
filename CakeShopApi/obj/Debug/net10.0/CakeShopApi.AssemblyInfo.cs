0010fbc4e2e73db3898c48dd859bd79ef4455ff6
Miyuru Tharuka <miyurut20@gmail.com>
2025-12-09 21:52:46 +0530

    fix

--- CakeShopApi/obj/Debug/net10.0/CakeShopApi.AssemblyInfo.cs
+++ CakeShopApi/obj/Debug/net10.0/CakeShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CakeShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257c4b49fe2b02fe442d6902a0dc37f230d6b35e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7e03bdb919da001fdeaf722a3eb9f22ca56219")]
 [assembly: System.Reflection.AssemblyProductAttribute("CakeShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CakeShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
